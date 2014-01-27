70180e11002029560a57dc7da17dd78167997250
Pedro <pedro.colon4@upr.edu>
2014-01-27 01:18:50 -0400

    Trying git push in webstorm

--- server.js
+++ server.js
@@ -8,10 +8,14 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 app.get('/search/:query', function(req, res){
-    res.send('Hello'+req.params.query);
+    console.log("Query is "+req.param.query);
+    res.send('Hello '+req.params.query);
 });
-
+app.get('/cat',function(req,res){
+    res.send('In cat');
+});
 var port = Number(process.env.PORT || 5000);
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
